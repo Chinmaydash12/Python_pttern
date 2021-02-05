2f41bd602c6a53343a4133a7988853fb55ca336d
Chinmaydash12 <78595116+Chinmaydash12@users.noreply.github.com>
2021-02-05 21:29:47 +0530

    Update py4p.py

--- py4p.py
+++ py4p.py
@@ -1,4 +1,4 @@
-Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32.....
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> d={0:24,1:'Hi','a':'Hello'}
 >>> d(a)
